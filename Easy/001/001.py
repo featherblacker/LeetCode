7509ed17009a32b34ca4bb5fea51a590a360a0d6
Qiguang Chu <chuqiguang@gmail.com>
2020-01-27 19:38:31 -0500

    sad

--- Easy/001/001.py
+++ Easy/001/001.py
@@ -9,3 +9,9 @@ class Solution(object):
             for j in range(i + 1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+    def twoSum1(self, nums, target):
+        res = []
+        for i in range(len(nums)):
+            if target-nums[i] in res:
+                return [i, res.index(target-nums[i])]
+            res.append(nums[i])
