dd458e488b87c6fe62f36011813991d365fe1511
Qiguang Chu <chuqiguang@gmail.com>
2020-01-28 17:37:16 -0500

    Update

--- Medium/279/279.py
+++ Medium/279/279.py
@@ -7,5 +7,6 @@ class Solution(object):
         dp = [0] + [float('inf')]*n
         for i in range(1, n+1):
             dp[i] = min(dp[i-j*j] for j in range(1, int(i**0.5)+1)) + 1
+            # Dynamic program that for each number find the minimum of all suqare numbers lower than it
         return dp[n]
         
